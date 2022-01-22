8460c3a850b235b9bb9f9630e2329fcfacdbdd80
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-22 14:03:39 -0500

    dbSchema setup

--- db/dbSchema.sql
+++ db/dbSchema.sql
@@ -16,3 +16,23 @@ USE employees;
 Create Table department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
+);
+    -- creat the roll table 
+    Create Table department (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(50) UNIQUE NOT NULL,
+        salary DECIMAL NOT NULL,
+        department_id INT NOT NULL,
+        CONSTRAINT fk_department FOREIGN KEY (department_id) References department(id) on delete cascade
+);
+    -- employees
+
+    Create Table department ( 
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(50) UNIQUE NOT NULL,
+        last_name DECIMAL NOT NULL,
+        role_id INT NOT NULL
+        manager_id INT,
+        CONSTRAINT fk_department FOREIGN KEY (manager_id) References employee(id) on delete set null
+    );
+
