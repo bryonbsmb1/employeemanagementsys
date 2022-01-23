a334aadbe27e76660730022951ef39869f2cf339
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-23 18:39:26 -0500

    mysql changes and root changes

--- db/connection.js
+++ db/connection.js
@@ -8,8 +8,8 @@ const connection = mysql.createConnection({
 //   this is where the Username is
 user: "root",
 // this is where the password is 
-password: "msubootcamp",
-database: "employees"
+password: "msubootcamp21",
+database: "bryon_employee_mgt_system"
 });
 connection.connect();
 connection.query =util.promisify(connection.query);
